88434ccb7e5c035c4635dd30b51d2d608745b85d
biao.du <biao.du@zongmutech.com>
2021-01-09 18:26:49 +0800

    web

--- vue_study/vue_study/src/components/Signin.vue
+++ vue_study/vue_study/src/components/Signin.vue
@@ -1,8 +1,10 @@
 <template>
-    <div class="signin">
+    <div class="singin">
         <h1>登录</h1>
         <v-text-field dense label="用户名"></v-text-field>
-        <v-text-field dense label="密码"></v-text-field>
+        <v-text-field dense label="密码" type='password'></v-text-field>
+        <v-btn color="warning" dark>登录</v-btn>
+        <v-btn color="primary" text>注册</v-btn>
     </div>
 </template>
 
@@ -14,8 +16,9 @@ export default {
 
 
 <style scoped>
-.signin{
-    width: 500px;
-    margin:　0  auto;
-    }
+    .singin{
+        width: 500px;
+        margin: 0 auto;
+        text-align: center;
+        }
 </style>
